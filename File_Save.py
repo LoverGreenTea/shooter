fbda542dff2a5f4e57e6ccd52baa00172fe774a5
LoverGreenTea <mgrican11@gmail.com>
2024-12-22 14:15:12 +0200

    1

--- File_Save.py
+++ File_Save.py
@@ -5,11 +5,11 @@ def save(data):
         with open('file_Save.json', 'r', encoding='utf-8') as file:
             data = json.load(file)
             return data
-        except:
-            return {
-                "scorer": 0,
-                "skin": "rocket.png"
-            }
+    except:
+        return {
+            "scorer": 0,
+            "skin": "rocket.png"
+        }
 
 def write_in_file(data):
     with open('file_save.json', 'w', encoding='utf=8') as file:
